85d7c167b027ddbd9b2db56cd90fca0419150daa
danny <danmigwi74@gmail.com>
2020-01-14 11:50:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/slidingtext/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.slidingtext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textview.isSelected = true
+    }
+}
